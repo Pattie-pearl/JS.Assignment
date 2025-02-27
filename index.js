bdb0324f96a68b9c0687e3f11a16d01dd628aeac
Pattie-pearl <pokpara2021@gmail.com>
2025-02-27 13:29:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//Declare a Variable
+
+let num1 = 11;
+let num2 = 4;
+
+//Perform Operations and Store the Results
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+//Print Results
+console.log("Addition:" + num1 + " + " + num2 + " = " + addition);
+console.log("Subtraction:" + num1 + " + " + num2 + " = " + subtraction);
+console.log("Multiplication:" + num1 + " + " + num2 + " = " + multiplication);
+console.log("Division:" + num1 + " + " + num2 + " = " + division);
+console.log("Modulus:" + num1 + " + " + num2 + " = " + modulus);
